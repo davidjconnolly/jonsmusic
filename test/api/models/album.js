0c4bdd7fe1ed35bc7ecb5efcb1b84fd8390140cb
David Connolly <david@twg.ca>
2014-09-10 11:28:16 -0400

    implemented skeleton for api testing

---
+++ test/api/models/album.js
@@ -0,0 +1,9 @@
+var Album = require('../../../api/models/album');
+
+describe('Album', function() {
+  describe(".parse()", function() {
+     it("should detect get album", function(){
+         expect(4+5).equal(9);
+     });
+   });
+});
---
+++ test/test_helper.js
@@ -0,0 +1,5 @@
+process.env.NODE_ENV = 'test'
+
+var expect = require("chai").expect;
+
+global.expect = expect
